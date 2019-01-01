8f08f036af5132d51696c8ca95f2a69307a29c22
rojoherrero <sergio@rojoherrero.es>
2019-01-01 14:18:44 +0100

    configuration not fetched from consul yet

--- resources/sql/dml.sql
+++ resources/sql/dml.sql
@@ -1,17 +1,17 @@
 --SAVE ROLE
-insert into accounts.roles(code, description) value ($1, $2);
+insert into accounts.roles(name, description) value ($1, $2);
 
 --GET ROLE BY CODE
-select r.code, r.description
+select r.name, r.description
 from accounts.roles r
-where r.code = $1;
+where r.name = $1;
 
-select r.code, r.description
+select r.name, r.description
 from accounts.roles r
-order by r.code asc
+order by r.name asc
 limit $1 offset $2;
 
 update accounts.roles
-set code = $1,
+set name = $1,
     description = $2
-where code = $3;
+where name = $3;
